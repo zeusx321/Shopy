de7c7397ed0cd80fee514b0f39de2e874ee19776
zeusx321 <ziadworks010@gmail.com>
2026-08-01 20:59:59 +0300

    feat: implement dynamic featured products section with remote data fetching and styling

--- js/featured.js
+++ js/featured.js
@@ -1,42 +1,49 @@
-let data;
 let categories;
 let categoriesDiv = document.querySelector(".categories");
 let arrowLeft = document.querySelector(".arrow-left");
 let arrowRight = document.querySelector(".arrow-right");
+let featuredProducts = document.querySelector(".featured-products")
 
-async function getCategories() {
+async function getPFroducts() {
   try {
-    const response = await fetch("https://dummyjson.com/products/categories");
+    const response = await fetch("https://dummyjson.com/products");
 
     if (!response.ok) {
       throw new Error(`HTTP Error: ${response.status}`);
     }
 
-    categories = await response.json();
+    data = await response.json();
     console.log(categories);
 
-    let categoriesHTML = "";
-    categories.forEach((item) => {
-      categoriesHTML += `<h3>${item.name}</h3>`;
-    });
+    const res = data.products.slice(7, 13);
 
-    categoriesDiv.insertAdjacentHTML("beforeend", categoriesHTML);
+    console.log(res)
+    
+    res.forEach((item) => {
+      featuredProducts.innerHTML += `
+        <div class="featured-product-div" key=${item.id}>
+          <div class="avaliable-product-div 
+          ${(item.availabilityStatus ==="In Stock")? "green-stock" : "low-stock"}">
+            <p>${item.availabilityStatus}</p>
+          </div>
+          <div class="category-product-div"><p>${item.category}</p></div>
+          <div class="product-img-div">
+            <img src=${item.thumbnail} alt=${item.title}>
+          </div>
+          <h3>${item.title}</h3>
+          <div class="brand-product-div"><p>${item.brand}</p></div>
+          <div class="description-product-div"><p>${item.description}</p></div>
+          <div class="price-product-div"><p>${item.price}$</p></div>
+          <button class="black-button product-button">Order Now</button>
+        </div>
+      `
+    })
+    
 
   } catch (error) {
     console.error(error);
   }
 }
 
-getCategories();
+getPFroducts();
 
-if (arrowLeft) {
-    arrowLeft.onclick = () => {
-        categoriesDiv.scrollBy({ left: -250, behavior: "smooth" });
-    };
-}
-
-if (arrowRight) {
-    arrowRight.onclick = () => {
-        categoriesDiv.scrollBy({ left: 250, behavior: "smooth" });
-    };
-}
